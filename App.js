7b28d834311fa79ff287a67e5082a633e180b0c0
Kleber Souza <kleberjfsouza@gmail.com>
2021-11-07 15:41:57 -0300

    .

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import HomePage from './src/pages/HomePage'
+
+const App = () => {
+
+  return (
+
+    <HomePage />
+
+  );
+
+}
+
+export default App;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const HomePage = () => {
+
+  return (
+
+    <View style={{flex:1, alignItems:'center', justifyContent:'center'}}> 
+
+      <Text> PUC Minas </Text>
+    
+    </View>
+
+  );
+
+}
+
+export default HomePage;
